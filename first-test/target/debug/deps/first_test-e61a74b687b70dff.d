0fa50341a2c6941092958032a2b532debb227760
Spidfire <spidfire@users.noreply.github.com>
2022-12-07 08:50:27 +0000

    current progress

---
+++ first-test/target/debug/deps/first_test-e61a74b687b70dff.d
@@ -0,0 +1,5 @@
+/workspaces/rust-codespace/first-test/target/debug/deps/first_test-e61a74b687b70dff: src/main.rs
+
+/workspaces/rust-codespace/first-test/target/debug/deps/first_test-e61a74b687b70dff.d: src/main.rs
+
+src/main.rs:
---
+++ first-test/target/debug/first-test.d
@@ -0,0 +1,1 @@
+/workspaces/rust-codespace/first-test/target/debug/first-test: /workspaces/rust-codespace/first-test/src/main.rs
